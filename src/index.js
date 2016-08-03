e82b269cea9a7e3e3f3fca01b9958280848aaf91
Max Rafferty <maxprafferty@gmail.com>
2016-08-03 15:58:02 -0400

    Added basic caching

--- src/index.js
+++ src/index.js
@@ -7,32 +7,62 @@ var request = require('request');
 var service = require('./boxService.js');
 var storage = require('./storage.js');
 var rollbarKeys = require('./rollbar.json');
+var AWS = require('aws-sdk');
+
+AWS.config.loadFromPath('./src/aws.json');
+var docClient = new AWS.DynamoDB.DocumentClient();
 
 // query the asset
 app.get(/^\/a\/{0,1}(.+)?/i, function (req, res) {
     'use strict';
+    var assetId;
+    var r;
+    var p;
+
+    var params = {
+        TableName: 'media-cache',
+        Key: {
+            'path': req.url
+        }
+    };
+
     log.debug(req.url);
     log.debug(req.params);
 
-    var assetId = req.params[0] || '0';
+    assetId = req.params[0] || '0';
     log.debug('Asset Id: ' + assetId);
 
-    var r;
-    var p = new Promise(resolve => {
+    p = new Promise(resolve => {
         r = data => {
             resolve(data);
         };
     });
 
     p.then(data => {
+        console.log('sneding data: ' + JSON.stringify(data));
         if (data) {
             res.send(data);
+            docClient.put({TableName: 'media-cache', Item: {path: req.url, data: data}}, function (err) {
+                if (err) {
+                    console.log('cache store failed: ' + err);
+                }
+            });
         } else {
             res.status(data.status || 404).send('Not Found');
         }
     });
 
-    service.getAssetInfo(assetId, r);
+    docClient.get(params, function (err, data) {
+        if (err || !Object.keys(data).length) {
+            console.log('cache miss for ' + req.url);
+            service.getAssetInfo(assetId, r);
+        } else {
+            console.log('cache hit for ' + req.url);
+            console.log('the data: ' + JSON.stringify(data.Item.data));
+            r(data.Item.data);
+        }
+    });
+
 });
 
 // Serve the asset
